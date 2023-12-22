ba42a2ed4123dc25a51e7d08c672d2fb6599e220
swr2photo <146337142+swr2photo@users.noreply.github.com>
2023-12-23 01:30:59 +0700

    Update script.js

--- upload/script.js
+++ upload/script.js
@@ -1,3 +1,22 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
 
 const form = document.querySelector("form"),
   fileInput = document.querySelector(".file-input"),
@@ -117,21 +136,4 @@ function handleUploadFailure(name) {
   progressArea.innerHTML = progressHTML;
 }
 
-/*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
 
-/* Menu show */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
-}
-
-/* Menu hidden */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
-}
